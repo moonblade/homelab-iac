ca9ced379d4a77291fc3d25cae1caa7ec453e435
mnishamk <mnishamk@commvault.com>
2025-01-05 17:30:41 +0530

    Update hostname from thinkcenter to athena

---
+++ athena/nixos/k3s/modules/k3s.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }: {
+
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = toString [
+      "--disable traefik"
+      "--disable servicelb"
+      "--disable metrics-server"
+      "--disable-cloud-controller"
+      "--kube-controller-manager-arg=--node-cidr-mask-size-ipv6=112"
+      "--kube-proxy-arg proxy-mode=ipvs"
+      "--cluster-cidr=10.42.0.0/16,2001:cafe:42::/96"
+      "--service-cidr=10.43.0.0/16,2001:cafe:43::/112"
+      "--snapshotter native"
+      "--disable-network-policy"
+      "--node-ip=192.168.1.151,fe80::ccc"
+      "--disable local-storage"
+      "--disable-helm-controller"
+      "--write-kubeconfig /home/operator/.kube/config"
+      "--flannel-backend=host-gw"
+      "--write-kubeconfig-mode 644"
+    ];
+  };
+
+}
