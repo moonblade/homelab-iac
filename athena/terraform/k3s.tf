1a176ce8519f1829d52579527fa326a9b7d73092
moonblade <moonblade168@gmail.com>
2025-02-09 16:41:47 +0530

    Change ram

--- athena/terraform/k3s.tf
+++ athena/terraform/k3s.tf
@@ -6,7 +6,7 @@ module "k3svm" {
   name        = "sirius"
   clone       = "nixos-base"
   cores       = 6
-  memory      = 8192
+  memory      = 12288
   desc        = "VM for k3s"
   sshkeys     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEvVn+sGksOE/YyWYo4meihsZxj3q7KPuzG2Yyfye7+H mb work lap"
   ipv4_addr   = "192.168.1.150/24"
