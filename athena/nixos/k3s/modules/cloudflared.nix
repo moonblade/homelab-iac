6085fbce579005be94ea7e1fa9b3d81a9eaffc09
moonblade <moonblade168@gmail.com>
2025-02-13 17:13:29 +0530

    update

--- athena/nixos/k3s/modules/cloudflared.nix
+++ athena/nixos/k3s/modules/cloudflared.nix
@@ -7,7 +7,7 @@
         credentialsFile = "/etc/nixos/k3s/secrets/46b0a967-3b0f-493a-88e7-6f1cc9f8852b.json";
         default = "http_status:404";
         ingress = {
-          "*.cf.moonblade.work" = {
+          "*.moonblade.work" = {
             service = "http://192.168.1.200";
           };
         };
