840630fdd252ac687f10df40263e8d4995a5797c
moonblade <moonblade168@gmail.com>
2026-02-22 14:49:21 +0530

    Fix cloudflared ingress ordering: use default for catch-all

--- athena/nixos/k3s/modules/cloudflared.nix
+++ athena/nixos/k3s/modules/cloudflared.nix
@@ -5,7 +5,7 @@
     tunnels = {
       "46b0a967-3b0f-493a-88e7-6f1cc9f8852b" = {
         credentialsFile = "/etc/nixos/k3s/secrets/46b0a967-3b0f-493a-88e7-6f1cc9f8852b.json";
-        default = "http_status:404";
+        default = "http://192.168.1.200";
         ingress = {
           "siriusk8s.moonblade.work" = {
             service = "https://127.0.0.1:6443";
@@ -18,9 +18,6 @@
           };
           "feeder.sirius.moonblade.work" = {
             service = "http://192.168.1.201";
-          };
-          "*.moonblade.work" = {
-            service = "http://192.168.1.200";
           };
           "moonblade.work" = {
             service = "http://192.168.1.200";
