94ef82bffb7b113495fa203ae00ab624ab38f31a
moonblade <moonblade168@gmail.com>
2026-02-23 09:39:49 +0530

    feat: switch cloudflared default to Gateway (192.168.1.201)
    - Default now routes to nginx-gateway-fabric (201) instead of ingress-nginx (200)
    - Explicitly list apps still on ingress-nginx for migration tracking
    - Migrated apps (feeder, ytssearch, abbsearch) now use default Gateway route

--- athena/nixos/k3s/modules/cloudflared.nix
+++ athena/nixos/k3s/modules/cloudflared.nix
@@ -5,7 +5,7 @@
     tunnels = {
       "46b0a967-3b0f-493a-88e7-6f1cc9f8852b" = {
         credentialsFile = "/etc/nixos/k3s/secrets/46b0a967-3b0f-493a-88e7-6f1cc9f8852b.json";
-        default = "http://192.168.1.200";
+        default = "http://192.168.1.201";
         ingress = {
           "siriusk8s.moonblade.work" = {
             service = "https://127.0.0.1:6443";
@@ -13,13 +13,44 @@
           "siriusssh.moonblade.work" = {
             service = "ssh://127.0.0.1:22";
           };
-          "feeder.moonblade.work" = {
-            service = "http://192.168.1.201";
+          # Still on ingress-nginx (192.168.1.200) - migrate these
+          "audiobook.moonblade.work" = {
+            service = "http://192.168.1.200";
           };
-          "feeder.sirius.moonblade.work" = {
-            service = "http://192.168.1.201";
+          "audiobooksearch.moonblade.work" = {
+            service = "http://192.168.1.200";
+          };
+          "browse.moonblade.work" = {
+            service = "http://192.168.1.200";
+          };
+          "ddsearch.moonblade.work" = {
+            service = "http://192.168.1.200";
+          };
+          "expense.moonblade.work" = {
+            service = "http://192.168.1.200";
+          };
+          "homer.moonblade.work" = {
+            service = "http://192.168.1.200";
+          };
+          "jackett.moonblade.work" = {
+            service = "http://192.168.1.200";
+          };
+          "jellyfin.moonblade.work" = {
+            service = "http://192.168.1.200";
+          };
+          "login.moonblade.work" = {
+            service = "http://192.168.1.200";
           };
           "moonblade.work" = {
+            service = "http://192.168.1.200";
+          };
+          "n8n.moonblade.work" = {
+            service = "http://192.168.1.200";
+          };
+          "transmission.moonblade.work" = {
+            service = "http://192.168.1.200";
+          };
+          "weave.moonblade.work" = {
             service = "http://192.168.1.200";
           };
         };
