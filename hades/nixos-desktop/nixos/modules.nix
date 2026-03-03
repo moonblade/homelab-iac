175f0447eb7db980104b0037ac77940ae30f1c9b
moonblade <moonblade168@gmail.com>
2026-03-03 22:53:48 +0530

    Add vim-style i3 keybindings (hjkl for navigation)

--- hades/nixos-desktop/nixos/modules.nix
+++ hades/nixos-desktop/nixos/modules.nix
@@ -8,6 +8,7 @@
   imports = [
     # Desktop Environment
     ./modules/desktop.nix      # X11 + i3 window manager
+    ./modules/i3config.nix     # i3 config with vim-style keybindings
     ./modules/xrdp.nix         # Remote desktop access
     ./modules/audio.nix        # PulseAudio (required for xrdp audio)
     
--- hades/nixos-desktop/nixos/modules/user.nix
+++ hades/nixos-desktop/nixos/modules/user.nix
@@ -50,10 +50,12 @@
 
     This is a NixOS desktop with i3 window manager.
     
-    Quick i3 shortcuts:
+    Quick i3 shortcuts (vim-style):
       Mod+Enter     - Open terminal (alacritty)
       Mod+d         - Application launcher (rofi)
       Mod+Shift+q   - Close focused window
+      Mod+h/j/k/l   - Focus left/down/up/right
+      Mod+Shift+h/j/k/l - Move window
       Mod+1-9       - Switch workspace
       Mod+Shift+e   - Exit i3
     
