701a689bba721b2842fc3015ca0822508f04a37d
moonblade <mnishamk95@gmail.com>
2026-04-29 21:08:13 +0530

    feat(sirius): increase VM RAM from 12GB to 20GB
    Needed to support gemma4:e2b model in Ollama (~10-12GB RAM).
    Applied directly via Proxmox qm set (Terraform state drift prevents
    safe plan/apply due to clone attribute forcing replacement).

--- hades/sirius/main.tf
+++ hades/sirius/main.tf
@@ -6,7 +6,7 @@ module "k3svm" {
   name        = "sirius"
   clone       = "nixos-base"
   cores       = 6
-  memory      = 12288
+  memory      = 20480
   balloon     = 0
   desc        = "VM for k3s"
   sshkeys     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEvVn+sGksOE/YyWYo4meihsZxj3q7KPuzG2Yyfye7+H mb work lap"
