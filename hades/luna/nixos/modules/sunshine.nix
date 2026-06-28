1d41cf4a2db29e602a7b78daa0166a37393b15e6
moonblade <mnishamk95@gmail.com>
2026-06-28 11:51:02 +0530

    fix: persist sunshine config, BusID xorg fix documented

--- hades/luna/nixos/modules/sunshine.nix
+++ hades/luna/nixos/modules/sunshine.nix
@@ -21,4 +21,23 @@
 
   # Add moonblade user to input group for uinput access
   users.users.moonblade.extraGroups = [ "input" "video" ];
+
+  # Persist Sunshine config across NixOS rebuilds
+  # output_name left unset = auto-detect first connected display (HDMI-0 dummy plug)
+  # nvenc fails on this GPU in KMS mode; Vulkan encoders (h264/hevc/av1_vulkan) work fine
+  environment.etc."sunshine/sunshine.conf".text = ''
+    min_bitrate = 20000
+    fps = 60
+    qp = 20
+    adapter_name = NVIDIA GeForce RTX 5060 Ti
+  '';
+
+  # Symlink config into place on activation
+  system.activationScripts.sunshineConfig = ''
+    mkdir -p /home/moonblade/.config/sunshine
+    if [ ! -f /home/moonblade/.config/sunshine/sunshine.conf ]; then
+      cp /etc/sunshine/sunshine.conf /home/moonblade/.config/sunshine/sunshine.conf
+      chown moonblade:users /home/moonblade/.config/sunshine/sunshine.conf
+    fi
+  '';
 }
